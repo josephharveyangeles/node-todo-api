5d05444879095ee766431a5eab387c67ad9117dc
yev <josephharveyangeles@gmail.com>
2018-08-23 11:48:55 +0800

    Add test cases for  GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,12 @@
 const expect = require('expect');
 const request = require('supertest');
-
+const {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [
-  {text: 'Fist test todo'},
-  {text: 'Second test todo'}
+  { _id: new ObjectID(), text: 'Fist test todo' },
+  { _id: new ObjectID(), text: 'Second test todo' }
 ]
 
 beforeEach(done => {
@@ -27,7 +27,7 @@ describe('POST /todos', () => {
       .expect(res => {
         expect(res.body.text).toBe(text);
       })
-      .end((err, res) => {
+      .end((err, res) => { // End block for testing internal state after the endpoint was called, as opposed to expect(res) where you test the actual response. The effect are both the same but it seems like it's more semantically correct to use end() for non-response related assertions.
         if (err) {
           return done(err);
         }
@@ -56,20 +56,47 @@ describe('POST /todos', () => {
             done();
           }).catch(e => done(e));
         });
-
     });
 
-});
-
-describe('GET /todos', () => {
+    
+  });
   
-  it('should get all todos', done => {
-    request(app)
+  describe('GET /todos', () => {
+    
+    it('should get all todos', done => {
+      request(app)
       .get('/todos')
       .expect(200)
       .expect(res => {
         expect(res.body.todos.length).toBe(2);
       }).end(done)
-  });
+    });
+    
+  })
+  
+  describe('GET /todos/:id', () => {
+    
+    it('should return a todo doc', (done) => {
+      request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+    });
+    
+    it('shoud return 400 if todo is non-existing', (done) => {
+      request(app)
+        .get(`/todos/${new ObjectID().toHexString()}`)
+        .expect(400)
+        .end(done);
+    });
 
+    it('shoud return 400 for invalid ids', (done) => {
+      request(app)
+        .get(`/todos/1234`)
+        .expect(400)
+        .end(done);
+    });
 })
